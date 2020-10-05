51a176e8d119e3479774550bbdb24c6b7f7877a1
Geoff Groenendale <ggroenendale@wsu.edu>
2020-10-05 10:00:14 -0700

    Recent updates

--- bodycomps/main.py
+++ bodycomps/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template
 
 from withings_api import WithingsAuth, WithingsApi, AuthScope
-from withings_api.common importget_measure_value, MeasureType
+from withings_api.common import get_measure_value, MeasureType
 
